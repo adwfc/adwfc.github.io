6e8402bfcafe99253388978254b336282a37fa62
ADWFC - Allgemeiner Deutscher Wanderer und Fahrradfahrer Club <jeremiasruff@tutanota.com>
2025-08-04 12:54:56 +0200

    Update sw.js

--- p/traum/sw.js
+++ p/traum/sw.js
@@ -1,10 +1,10 @@
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => console.log('Service Worker aktiviert'));
+self.addEventListener('activate', () => console.log('SW aktiv'));
 
 self.addEventListener('message', (event) => {
   if (event.data === 'note-update') {
-    self.registration.showNotification('Notiz aktualisiert ✏️', {
-      body: 'Notiz aktualisiert.',
+    self.registration.showNotification('Notiz aktualisiert', {
+      body: 'Jemand hat eine Notiz bearbeitet.',
       icon: 'm/rakete.png'
     });
   }
